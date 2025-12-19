3614f9dcd345761c854c4cb034f9e13329de3871
Franco Natanael Tomaino <tomainofranco5@gmail.com>
2025-12-19 11:07:53 -0300

    Initial commit with supabase config

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import { DeployButton } from "@/components/deploy-button";
+import { AuthButton } from "@/components/auth-button";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { hasEnvVars } from "@/lib/utils";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/"}>Lerny</Link>
+            </div>
+            {!hasEnvVars ? (
+              <div>Missing environment variables</div>
+            ) : (
+              <Suspense>
+                <AuthButton />
+              </Suspense>
+            )}
+          </div>
+        </nav>
+      </div>
+    </main>
+  );
+}
